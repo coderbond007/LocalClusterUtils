d253cbc461c67f9a58f06cd4c3d0b79585237564
pradyumnag <pradyumn.ag@media.net>
2019-11-23 15:38:19 +0530

    [ADDED]: Added few parsers and data shifter

--- src/main/java/hive/DataShifter.java
+++ src/main/java/hive/DataShifter.java
@@ -1,14 +1,20 @@
 package hive;
 
+import static org.apache.spark.sql.functions.lit;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
+import org.apache.thrift.TException;
 import scala.collection.JavaConverters;
 import scala.collection.Seq;
 import spark.SparkClient;
@@ -20,42 +26,17 @@ public class DataShifter {
   private static final int LIMIT = 5;
   private static SparkSession sparkSession;
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+      throws InterruptedException, AnalysisException, TException {
     sparkSession = SparkClient.getClient();
 //    deleteData(sparkSession);
-    Dataset<Row> datasetSrc = sparkSession.table(TABLE_SRC);
-//    datasetSrc = datasetSrc.withColumn("label", lit(1));
-//    Map<String, String> aggFunc = new HashMap<String, String>();
-//    aggFunc.put("label", "sum");
-//    datasetSrc = datasetSrc.groupBy("ts").agg(aggFunc);
+    Dataset<Row> datasetSrc = sparkSession.table(TABLE_DEST);
+    datasetSrc = datasetSrc.withColumn("label", lit(1));
+    Map<String, String> aggFunc = new HashMap<String, String>();
+    aggFunc.put("label", "sum");
+    datasetSrc = datasetSrc.groupBy("ts").agg(aggFunc);
     datasetSrc.show();
-//    datasetSrc.write().insertInto(TABLE_DEST);
-//    StructType targetTableStructType = getModifiedStruct(datasetSrc);
-//    PrintWriter out = new PrintWriter(new FileOutputStream(new File(
-//        "/Users/pradyumn.ag/IdeaProjects/LocalClusterUtils/data/hive/queries/create_query_without_array.sql")));
-
-//    out.println("INSERT INTO " + TABLE_DEST);
-//    out.println("(SELECT ");
-//    int colSize = targetTableStructType.size();
-//    for (int i = 0; i < colSize; ++i) {
-//      out.print(targetTableStructType.fieldNames()[i]);
-//      if (i < colSize - 1) {
-//        out.print(",");
-//      }
-//      out.println();
-//    }
-//    out.flush();
-//    out.println("FROM ");
-//    out.println(TABLE_SRC);
-//    out.println(")");
-//    out.close();
-
-//    Dataset<Row> finDataset = sparkSession.table(TABLE_DEST);
-//    finDataset.show(LIMIT);
-//    datasetSrc.write().insertInto(TABLE_DEST);
-
-//    SparkClient.getClient().sql("TRUNCATE TABLE " + TABLE_DEST);
-//    datasetTarget.show(LIMIT);
+    Thread.sleep(1000_000);
   }
 
   private static void deleteData(SparkSession sparkSession) {
--- src/main/java/spark/Main.java
+++ src/main/java/spark/Main.java
@@ -1,23 +1,27 @@
 package spark;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.util.List;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.catalyst.parser.ParseException;
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
-  private static final String QUERY = "SELECT abtest_bucket_id.value AS `dimension467`, Coalesce(Concat(bucket_bucket_id.`value`, \"- [\", `bucket_id`, \"]\"), `bucket_id`) AS `dimension527`, Coalesce(Concat(device_device_id.`value`, \"- [\", `device_id`, \"]\"), `device_id` ) AS `dimension586`, Sum(CASE WHEN(( `is_container` NOT IN ( '0' ) OR Isnull(`is_container`) )) THEN Coalesce(`views`, 0) ELSE NULL end) `filteredmetric1001758` FROM web_log.page_view LEFT JOIN analytics_master.abtest AS abtest_bucket_id ON abtest_bucket_id.key = `bucket_id` LEFT JOIN analytics_master.device AS device_device_id ON device_device_id.key = `device_id` LEFT JOIN analytics_master.bucket AS bucket_bucket_id ON bucket_bucket_id.key = `bucket_id` WHERE (( abtest_bucket_id.value IN ( '74204 : E2_SERP PROMPT', '74216 : E2_SERP Prompt_Batch 2', '74218 : E2_PushPrompt_Batch 3', '74220 : Premium_Serp Prompt_US', '74156 : E1_US SERP Prompt' ) )) GROUP BY Coalesce(Concat(bucket_bucket_id.`value`, \"- [\", `bucket_id`, \"]\"), `bucket_id` ), abtest_bucket_id.`value`, Coalesce(Concat(device_device_id.`value`, \"- [\", `device_id`, \"]\"), `device_id` ) ";
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+  private static final String QUERY = "SELECT abtest_bucket_id.value AS `dimension467`, Coalesce(Concat(bucket_bucket_id.`value`, \"- [\", `bucket_id`, \"]\"), `bucket_id`) AS `dimension527`, Coalesce(Concat(device_device_id.`value`, \"- [\", `device_id`, \"]\"), `device_id` ) AS `dimension586`, Sum(CASE WHEN(( `is_container` NOT IN ( '0' ) OR Isnull(`is_container`) )) THEN Coalesce(`views`, 0) ELSE NULL end) `filteredmetric1001758` FROM web_log.page_view LEFT JOIN analytics_master.abtest AS abtest_bucket_id ON abtest_bucket_id.key = `bucket_id` LEFT JOIN analytics_master.device AS device_device_id ON device_device_id.key = `device_id` LEFT JOIN analytics_master.bucket AS bucket_bucket_id ON bucket_bucket_id.key = `bucket_id` WHERE ts='2019100101' and (( abtest_bucket_id.value IN ( '74204 : E2_SERP PROMPT', '74216 : E2_SERP Prompt_Batch 2', '74218 : E2_PushPrompt_Batch 3', '74220 : Premium_Serp Prompt_US', '74156 : E1_US SERP Prompt' ) )) GROUP BY Coalesce(Concat(bucket_bucket_id.`value`, \"- [\", `bucket_id`, \"]\"), `bucket_id` ), abtest_bucket_id.`value`, Coalesce(Concat(device_device_id.`value`, \"- [\", `device_id`, \"]\"), `device_id` ) ";
   private static final String DASH_LINE = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
 
   public static void main(String[] args) throws ParseException, FileNotFoundException {
+    LOGGER.error("Failed");
     SparkSession sparkSession = SparkClient.getClient();
+//    LogicalPlan logicalPlan = sparkSession.sql(QUERY).queryExecution().optimizedPlan();
+//    Dataset<Row> dataset = sparkSession.sql(QUERY);
+    LOGGER.info("Inside Main Program");
+    LogicalPlan logicalPlan = sparkSession.sessionState().sqlParser().parsePlan(QUERY);
+    System.out.println(logicalPlan.treeString());
+//    dataset.explain(true);
 //    SessionState sessionState = sparkSession.sessionState();
 //    LogicalPlan logicalPlan = sessionState.sqlParser().parsePlan(QUERY);
 //    QueryExecution queryExecution = sessionState.executePlan(logicalPlan);
@@ -29,12 +33,12 @@ public class Main {
 //    SparkPlan sparkPlan1 = queryExecution.sparkPlan();
 //    System.out.println(sparkPlan1.treeString());
 //    doWithOptiPlan(optimizedPlan);
-    Dataset<Row> dataset = sparkSession.sql("SHOW CREATE TABLE `default`.`page_view_csv`");
-    List<Row> rows = dataset.collectAsList();
-    PrintWriter out = new PrintWriter(new FileOutputStream(new File(
-        "/Users/pradyumn.ag/IdeaProjects/LocalClusterUtils/data/hive/queries/create_query_without_array.sql")));
-    out.println(rows.get(0));
-    out.flush();
+//    Dataset<Row> dataset = sparkSession.sql("SHOW CREATE TABLE `default`.`page_view_csv`");
+//    List<Row> rows = dataset.collectAsList();
+//    PrintWriter out = new PrintWriter(new FileOutputStream(new File(
+//        "/Users/pradyumn.ag/IdeaProjects/LocalClusterUtils/data/hive/queries/create_query_without_array.sql")));
+//    out.println(rows.get(0));
+//    out.flush();
   }
 
   private static void doWithOptiPlan(LogicalPlan optimizedPlan) {
